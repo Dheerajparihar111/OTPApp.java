72cc05225c2c4ffefd80fb931ff01a888c7cbd43
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-02-15 21:46:20 +0530

    added randomly generated options and secure generated otp

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.sql.SQLOutput;
+import java.util.*;
+import java.security.SecureRandom;
+class Homepage{
+void  Homepage(){
+        System.out.println("Enter your choice");
+        System.out.println("1) Randomly Generated OTP");
+        System.out.println("2) Secure OTP");
+        System.out.println("3) Exit");
+    }
+}
+public class Main {
+    static void main(String[] args) {
+        Random random = new Random();
+        SecureRandom secureRandom = new SecureRandom();
+        Homepage homepage = new Homepage();
+        Scanner input = new Scanner(System.in);
+        System.out.println("***********Wel-Come To OTP App*********");
+        while (true) {
+            homepage.Homepage();
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    int OTP = random.nextInt(10000);
+                    System.out.println("The Generated OTP:" + OTP);
+                    break;
+                case 2:
+                    int otp = secureRandom.nextInt(10000);
+                    System.out.println("The  Generated OTP:" + otp);
+                    break;
+                case 3:
+                    System.out.println("Thank You For Using OTP generating App");
+                    System.exit(0);
+            }
+        }
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    }
 }
